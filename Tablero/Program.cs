186ea158b989e33d31db9e78066a60ef975033ea
Alejandro Obando Cano <alejandro_obando@outlook.com>
2020-02-23 23:56:09 -0400

    Add project files.

---
+++ Tablero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tablero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tab;
+            char esp = 'X';
+           for (int i=0;i<8;i++)
+            {
+                tab = null;
+                for (int j=0; j<8; j++)
+                {
+                    tab += esp;
+                    esp = Cambiar_Espacio(esp);
+                }
+                Console.WriteLine(tab);
+                esp = Cambiar_Espacio(esp);
+            }
+            Console.ReadKey();
+        }
+
+        private static char Cambiar_Espacio(char esp)
+        {
+            if (esp == 'O')
+                esp = 'X';
+            else
+                esp = 'O';
+            return esp;
+        }
+    }
+}
